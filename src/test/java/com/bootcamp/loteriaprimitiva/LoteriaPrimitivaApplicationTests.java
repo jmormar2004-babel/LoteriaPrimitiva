d1d3b2827d97123a969507250d0b244cd2ec9064
jmormar2004-babel <juan.mmarquez@babelgroup.com>
2024-04-18 10:57:33 +0200

    Inicio del repositorio

---
+++ src/test/java/com/bootcamp/loteriaprimitiva/LoteriaPrimitivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.loteriaprimitiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriaPrimitivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
