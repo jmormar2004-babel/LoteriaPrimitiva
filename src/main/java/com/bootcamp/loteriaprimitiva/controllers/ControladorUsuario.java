57f630b2c21df889648f29ebc2a56752fd353aed
jmormar2004-babel <juan.mmarquez@babelgroup.com>
2024-04-18 12:14:31 +0200

    implementación de códigos HTTP en el controlador.

--- src/main/java/com/bootcamp/loteriaprimitiva/controllers/ControladorUsuario.java
+++ src/main/java/com/bootcamp/loteriaprimitiva/controllers/ControladorUsuario.java
@@ -2,10 +2,13 @@ package com.bootcamp.loteriaprimitiva.controllers;
 
 import com.bootcamp.loteriaprimitiva.models.Usuario;
 import com.bootcamp.loteriaprimitiva.services.IServicioUsuarios;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/users")
@@ -18,23 +21,41 @@ public class ControladorUsuario {
     }
 
     @PutMapping("/{id}")
-    public void registrarUsuario(@PathVariable String id, @RequestBody Usuario usuario){
-        this.servicioUsuarios.agregarUsuario(usuario, id);
+    public ResponseEntity<String> registrarUsuario(@PathVariable String id, @RequestBody Usuario usuario){
+        if(this.servicioUsuarios.agregarUsuario(usuario, id)){
+            return ResponseEntity.status(CREATED).body("Usuario Registrado");
+        }
+        return ResponseEntity.status(CONFLICT).body("El usuario ya existe en la base de datos");
     }
 
     @GetMapping
-    public List<Usuario> listarUsuarios(){
-        return this.servicioUsuarios.listarUsuarios();
+    public ResponseEntity<List<Usuario>> listarUsuarios(){
+        return ResponseEntity.status(OK).body(this.servicioUsuarios.listarUsuarios());
     }
 
     @GetMapping("/{id}")
-    public Usuario getUsuario(@PathVariable String id){
-        return this.servicioUsuarios.obtenerUsuario(id);
+    public ResponseEntity<Usuario> getUsuario(@PathVariable String id){
+        if(this.servicioUsuarios.obtenerUsuario(id) == null) return ResponseEntity.status(NOT_FOUND).body(null);
+        return ResponseEntity.status(OK).body(this.servicioUsuarios.obtenerUsuario(id));
     }
 
     @PostMapping("/{id}/bets")
-    public void registrarApuesta(@PathVariable String id, @RequestBody Set<Integer> apuesta){
-        this.servicioUsuarios.registrarApuesta(id, apuesta);
+    public ResponseEntity<String> registrarApuesta(@PathVariable String id, @RequestBody Set<Integer> apuesta){
+        switch (this.servicioUsuarios.registrarApuesta(id, apuesta)){
+            case 0 -> {
+                return ResponseEntity.status(CREATED).body("Apuesta agregada");
+            }
+            case 1 -> {
+                return ResponseEntity.status(BAD_REQUEST).body("La apuesta porque tiene números mayor a 49.");
+            }
+            case 2 -> {
+                return ResponseEntity.status(NOT_FOUND).body("El usuario no ha sido encontrado.");
+            }
+            case 3 -> {
+                return ResponseEntity.status(BAD_REQUEST).body("El tamaño de la apuesta debe ser 6.");
+            }
+        }
+        return null;
     }
 
 }
